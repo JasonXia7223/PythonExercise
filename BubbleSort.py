3255ddf7eeaef696473f12266f87936f3cd53f0f
JasonXia7223 <JasonXia_7223@outlook.com>
2016-11-21 23:04:47 +0800

    Bubble Sort finished,  insert sort starting

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,19 +1,20 @@
 #Bubble Sort
 
-list_Int = [8, 2, 4, 6, 1, 9, 0, 3, 5, 7]
+list_Int = [8, 2, 4, 6, 1, 9, 0, 3, 5, 7, 9]
 max_Len = len(list_Int)
 i = max_Len
 list_Index = max_Len
 
-while list_Index > 0:
-    for i in range(max_Len, 5, -1):
+while list_Index > 1:
+    for i in range(max_Len - 1, 0, -1):
+
         if list_Int[i - 1] < list_Int[i]:
             list_Int[i] = list_Int[i -1] + list_Int[i]
             list_Int[i - 1] = list_Int[i] - list_Int[i - 1]
             list_Int[i] = list_Int[i] - list_Int[i - 1]
-
-    print list_Index, max_Len, list_Int
     list_Index -= 1
+    print list_Index, i, list_Int
+# print list_Index, i, list_Int
 
 
 
